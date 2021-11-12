23186d89cb9cb7e86a74ae6b08c7c49a3a758a4d
ra-gh-ul <raghulselvam.97@gmail.com>
2021-11-12 14:59:51 +0530

    testone

---
+++ src/org/company/Company.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class Company {
+
+	public void cmpId() {
+
+	}
+
+	public void cmpAddress() {
+
+	}
+
+	public static void main(String[] args) {
+
+		Company c = new Company();
+		c.cmpId();
+		c.cmpAddress();
+
+	}
+}
